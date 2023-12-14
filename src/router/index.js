c7a991034cde178c39be80138d0455464a05fca0
webclidev <webclidev@gmail.com>
2023-12-14 21:27:09 +0530

    added new route for individual note

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ViewNotes from "@/views/ViewNotes.vue";
+import ViewNote from "@/views/ViewNote.vue";
 import ViewStats from "@/views/ViewStats.vue";
 
 const routes = [
   { path: "/", name: "notes", component: ViewNotes },
+  { path: "/:id", name: "note", component: ViewNote },
   { path: "/stats", name: "stats", component: ViewStats },
 ];
 
---
+++ src/views/ViewNote.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <SingleNote />
+  </v-container>
+</template>
+
+<script setup>
+import SingleNote from "@/components/SingleNote.vue";
+</script>
