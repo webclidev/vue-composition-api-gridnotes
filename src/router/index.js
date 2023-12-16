00b13860bcfe00247a327df33c121814f8fccbbc
webclidev <webclidev@gmail.com>
2023-12-16 10:10:24 +0530

    added <ViewEditNote/>

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ViewNotes from "@/views/ViewNotes.vue";
+import ViewEditNote from "@/views/ViewEditNote.vue";
 import ViewStats from "@/views/ViewStats.vue";
 
 const routes = [
   { path: "/", name: "notes", component: ViewNotes },
+  { path: "/edit-note/:id", name: "editNote", component: ViewEditNote },
   { path: "/stats", name: "stats", component: ViewStats },
 ];
 
---
+++ src/views/ViewEditNote.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <NoteEditor
+      class="mb-6"
+      v-model:textarea="noteText"
+      ref="noteTextRef"
+      placeholder="Edit note"
+    >
+      <template #action>
+        <v-btn
+          variant="tonal"
+          prepend-icon="mdi-note-edit-outline"
+          class="text-capitalize px-5"
+          @click="editNoteHandler"
+          :disabled="!noteText.trim()"
+          >Edit note</v-btn
+        >
+      </template>
+    </NoteEditor>
+  </v-container>
+</template>
+
+<script setup>
+import NoteEditor from "@/components/NoteEditor.vue";
+import { useNotesStore } from "@/store/notesStore.js";
+import { ref } from "vue";
+
+const noteText = ref("");
+const noteTextRef = ref(null);
+
+const notesStore = useNotesStore();
+
+const editNoteHandler = () => {};
+</script>
