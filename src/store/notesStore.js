f7e7232ffb94c755e0931b91b23cb8e26cc2bb87
webclidev <webclidev@gmail.com>
2023-12-23 15:30:50 +0530

    feat: Add useLocalStorage composable for storing notes in localStorage

--- src/store/notesStore.js
+++ src/store/notesStore.js
@@ -1,19 +1,9 @@
 import { defineStore } from "pinia";
+import { useLocalStorage } from "@vueuse/core";
 
 export const useNotesStore = defineStore("notes", {
   state: () => ({
-    notes: [
-      {
-        id: "ef8772a9-85e8-5840-9d0d-4812d357b2de",
-        content:
-          "means busy help open capital storm time nodded driver sides gasoline percent arrange manner occasionally captain musical share pocket escape related building available shown",
-      },
-      {
-        id: "4922760d-1863-50e2-8c4c-27a096fef55a",
-        content:
-          "suggest whether molecular part some over furniture suddenly southern serve citizen were upper region doing surface arrangement slightly dangerous victory edge fine heard row",
-      },
-    ],
+    notes: useLocalStorage("notes", []),
   }),
 
   getters: {
